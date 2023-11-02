2960110b1b64eb5c0cc871e264b2b36d24338948
DeboraChiarelli <deborachiarelli92@gmail.com>
2023-11-02 15:56:26 +0100

    Add - v-for in index.html

--- js/app.js
+++ js/app.js
@@ -4,4 +4,38 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 PS.
-Ricordatevi di importare axios! https://axios-http.com/docs/intro */ 
+Ricordatevi di importare axios! https://axios-http.com/docs/intro */
+
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            emails: [],
+        }
+    },
+    methods: {
+        fetchEmail() {
+            for (let i = 0; i < 10; i++) {
+                axios 
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((res) => {
+                    const email = res.data.response;
+                    this.emails.push(email);
+                })
+            }
+        }
+    },
+    created() {
+        this.fetchEmail()
+    },
+}).mount('#app')
+
+/* 
+axios 
+.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then((res) => {
+const email = res.data.response;
+this.emails.push(email);
+}
+)
+*/
